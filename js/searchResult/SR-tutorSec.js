f6e5449e8a72e60663a6f2c42787f90e60bf48a7
Delgermaa1230 <galbayardelgermaa@gmail.com>
2024-12-18 12:56:30 +0800

    love button

--- js/searchResult/SR-tutorSec.js
+++ js/searchResult/SR-tutorSec.js
@@ -33,10 +33,9 @@ export default class tutor {
                             stroke-linecap="round"
                             stroke-linejoin="round"
                             class="heart-icon"
-                            style="width: 24px; height: 24px;"
-                >
-                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 20.49l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                </svg></button>
+                            style="width: 24px; height: 24px;">
+                            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 20.49l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" fill="rgba(255, 255, 255, 0.5)"></path>
+                        </svg></button>
                     </div>
                     <h3>${firstLetterOfLastName}. ${this.ner}</h3>
                     <p class="tutorReviews">
