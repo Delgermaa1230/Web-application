9ae5743a346b2664ff0b3be6ee1288c347e414f8
bataa <bataabatka92@gmail.com>
2025-01-02 16:53:40 +0800

    aa

--- js/searchResult/SR-APP.js
+++ js/searchResult/SR-APP.js
@@ -2,11 +2,14 @@ import tutorSec ,{loadData} from "./SR-tutorSec.js";
 
 const tutorData = await loadData();
 
+function menufilter(tutorData,){
+    return tutorData.filter()
+
+}
 const Tutorhtml = 
     tutorData
     .map(td =>(new tutorSec(td)).render())
-    .reduce((p,c)=>p+c)
-    .
+    .reduce((p,c)=>p+c);
 
 
 document.getElementById("tutors").insertAdjacentHTML("beforeend",Tutorhtml)
