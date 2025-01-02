842ca432e3bb9b1b713a0f3892d25c5b4c8858f3
bataa <bataabatka92@gmail.com>
2025-01-02 19:20:05 +0800

    aa

--- js/searchResult/SR-APP.js
+++ js/searchResult/SR-APP.js
@@ -2,14 +2,33 @@ import tutorSec ,{loadData} from "./SR-tutorSec.js";
 
 const tutorData = await loadData();
 
-function menufilter(tutorData,){
-    return tutorData.filter()
-
-}
-const Tutorhtml = 
-    tutorData
+function renderTutors(filteredData){
+    const Tutorhtml = 
+    filteredData
     .map(td =>(new tutorSec(td)).render())
-    .reduce((p,c)=>p+c);
+    .reduce((p,c)=>p+c);    
 
+document.getElementById("tutors").insertAdjacentHTML("beforeend",Tutorhtml);
+}
 
-document.getElementById("tutors").insertAdjacentHTML("beforeend",Tutorhtml)
+renderTutors(tutorData);
+
+function filterTutors(criteria){
+    let filteredData;
+
+    if(criteria === "all"){
+        filteredData = tutorData;
+    }else {
+        filteredData = tutorData.filter(tutor => tutor.unelgee === criteria)
+    }
+
+    renderTutors(filteredData);
+}
+
+document.getElementById("all").addEventListener("click", () => filterTutors("all"))
+document.getElementById("1").addEventListener("click", () => filterTutors("1"))
+document.getElementById("2").addEventListener("click", () => filterTutors("2"))
+document.getElementById("3").addEventListener("click", () => filterTutors("3"))
+document.getElementById("4").addEventListener("click", () => filterTutors("4"))
+document.getElementById("5").addEventListener("click", () => filterTutors("5"))
+
